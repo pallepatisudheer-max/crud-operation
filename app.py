7f3594d3c72dda614328183a4b14dc576177e9f5
sudheer2-lab <pallepatisudheer96@gmail.com>
2026-06-03 11:01:22 +0530

    Added SQLite database and SQLAlchemy ORM

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, request, redirect
+from db import db
+from models import Student
 
 app = Flask(__name__)
 
-students = []
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///s.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
+    students = Student.query.all()
     return render_template('index.html', students=students)
 
 @app.route('/add', methods=['POST'])
@@ -13,29 +22,49 @@ def add():
     name = request.form['name']
     age = request.form['age']
 
-    students.append({
-        'name': name,
-        'age': age
-    })
+    student = Student(
+        name=name,
+        age=age
+    )
+
+    db.session.add(student)
+    db.session.commit()
 
     return redirect('/')
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    if id < len(students):
-        students.pop(id)
+    student = Student.query.get_or_404(id)
+
+    db.session.delete(student)
+    db.session.commit()
+
     return redirect('/')
 
 @app.route('/edit/<int:id>')
 def edit(id):
-    student = students[id]
-    return render_template('edit.html', student=student, id=id)
+    student = Student.query.get_or_404(id)
+    return render_template('edit.html', student=student)
 
 @app.route('/update/<int:id>', methods=['POST'])
 def update(id):
-    students[id]['name'] = request.form['name']
-    students[id]['age'] = request.form['age']
-    return redirect('/')
+    student = Student.query.get_or_404(id)
 
+    student.name = request.form['name']
+    student.age = request.form['age']
+
+    db.session.commit()
+
+    return redirect('/')
+@app.route('/database')
+def database():
+    students = Student.query.all()
+
+    result = ""
+
+    for student in students:
+        result += f"ID: {student.id}, Name: {student.name}, Age: {student.age}<br>"
+
+    return result
 if __name__ == '__main__':
     app.run(debug=True)
